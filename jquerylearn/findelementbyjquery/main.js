ee0b715e60becfa57253d2bd80152748f87baa6d
skyADMIN <546485299@qq.com>
2016-04-22 13:39:44 +0800

    引入jQuery文件，编写主程序代码。查找元素并替换内容。

---
+++ jquerylearn/findelementbyjquery/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by skyADMIN on 16/4/22.
+ */
+
+$(document).ready(function ($) {
+    $("h1"); //find h1 element in index.html
+    
+    $("h1").text("呵呵哒"); //change h1's text by text() method
+    
+    
+});
